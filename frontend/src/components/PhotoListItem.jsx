bca5d8861bdc5f6667b5bc74857fe13d5a7b8a99
Brendan Brown <brendan94brown@gmail.com>
2025-05-16 15:50:10 -0700

    Fix prop references so that pictures, user profile, and user names are rendered correctly

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -6,11 +6,11 @@ const PhotoListItem = (props) => {
 
   return (
     <div className="photo-list__item">
-      <img className="photo-list__image" src={props.photo.imageSource} alt="Main" />
+      <img className="photo-list__image" src={props.photo.urls.regular} alt="Main" />
       <div className="photo-list__user-details">
-        <img className="photo-list__user-profile" src={props.photo.profile} alt="User profile" />
+        <img className="photo-list__user-profile" src={props.photo.user.profile} alt="User profile" />
         <div className="photo-list__user-info">
-          <p>{props.photo.username}</p>
+          <p>{props.photo.user.name}</p>
           <p>
             {props.photo.location.city},
             {props.photo.location.country}
