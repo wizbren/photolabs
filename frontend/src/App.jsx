03a799aa891cefeea69f1f03bda6fda4c646497d
Brendan Brown <brendan94brown@gmail.com>
2025-05-16 15:36:27 -0700

    Create array of 3 photos with slice and map methods, instead of just rendering 1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PhotoListItem from './components/PhotoListItem';
+import photos from './mocks/photos';
 import './App.scss';
 
 
@@ -14,11 +15,15 @@ const sampleDataForPhotoListItem = {
   profile: "/profile-1.jpg",
 };
 
+const threePhotos = photos.slice(0, 3);
+
 
 const App = () => {
   return (
     <div className="App">
-      <PhotoListItem photo={sampleDataForPhotoListItem}/>
+      {photos.slice(0, 3).map((photo) => (
+        <PhotoListItem key={photo.id} photo={photo} />
+      ))}
     </div>
   );
 };
