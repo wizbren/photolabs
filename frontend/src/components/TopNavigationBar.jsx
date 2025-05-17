c48df9c1b287d52f7cc3bbc4a3e9cbb27f015dad
Brendan Brown <brendan94brown@gmail.com>
2025-05-17 12:29:40 -0700

    Import and render TopicList and FavBadge into nav bar component

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -1,9 +1,13 @@
 import '../styles/TopNavigationBar.scss'
+import TopicList from './TopicList';
+import FavBadge from './FavBadge';
 
 const TopNavigationBar = () => {
   return (
     <div className="top-nav-bar">
       <span className="top-nav-bar__logo">PhotoLabs</span>
+      <TopicList />
+      <FavBadge />
     </div>
   )
 };
