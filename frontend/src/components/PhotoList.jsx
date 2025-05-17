fc7d4dcf867614309a09122b44a55df2597e7210
Brendan Brown <brendan94brown@gmail.com>
2025-05-16 18:00:45 -0700

    Fill out PhotoList component for displaying list of PhotoListItem components

--- frontend/src/components/PhotoList.jsx
+++ frontend/src/components/PhotoList.jsx
@@ -1,62 +1,76 @@
-import "../styles/PhotoList.scss";
+// frontend/src/components/PhotoList.jsx
+import React from 'react';
+import PhotoListItem from './PhotoListItem';
+import '../styles/PhotoList.scss';
+
 
 const sampleDataForPhotoList = [
   {
-    id: 1,
+    id: "1",
     location: {
       city: "Montreal",
       country: "Canada",
     },
     urls: {
-      full: "/Image-1-Full.jpeg",
-      regular: "/Image-1-Regular.jpeg",
+      full: `${process.env.PUBLIC_URL}/Image-1-Full.jpeg`,
+      regular: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
     },
     user: {
+      id: "1",
       username: "exampleuser",
       name: "Joe Example",
-      profile: "/profile-1.jpg",
+      profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
     },
   },
   {
-    id: 2,
+    id: "2",
     location: {
       city: "Toronto",
       country: "Canada",
     },
     urls: {
-      full: "/Image-2-Full.jpeg",
-      regular: "/Image-2-Regular.jpeg",
+      full: `${process.env.PUBLIC_URL}/Image-2-Full.jpeg`,
+      regular: `${process.env.PUBLIC_URL}/Image-2-Regular.jpeg`,
     },
     user: {
+      id: "2",
       username: "exampleuser",
       name: "Joe Example",
-      profile: "/profile-1.jpg",
+      profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
     },
   },
   {
-    id: 3,
+    id: "3",
     location: {
       city: "Ottawa",
       country: "Canada",
     },
     urls: {
-      full: "/Image-3-Full.jpeg",
-      regular: "/Image-3-Regular.jpeg",
+      full: `${process.env.PUBLIC_URL}/Image-3-Full.jpeg`,
+      regular: `${process.env.PUBLIC_URL}/Image-3-Regular.jpeg`,
     },
     user: {
+      id: "3",
       username: "exampleuser",
       name: "Joe Example",
-      profile: "/profile-1.jpg",
+      profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
     },
   },
 ];
 
+
+
 const PhotoList = () => {
+
   return (
     <ul className="photo-list">
-      {/* Insert React */}
+      {sampleDataForPhotoList.map((photo) => (
+        <li key={photo.id}>
+          <PhotoListItem photo={photo} />
+        </li>
+      ))}
     </ul>
   );
 };
 
-export default PhotoList;
+export default PhotoList
