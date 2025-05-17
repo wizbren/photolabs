f5184db608d2c261d2419f62c672bb29f39d7fb8
Brendan Brown <brendan94brown@gmail.com>
2025-05-16 18:01:25 -0700

    Remove 2 unnecessary imports as they are no longer being used, and add PhotoList component into App container

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,13 @@
 import React from 'react';
-import PhotoListItem from './components/PhotoListItem';
-import photos from './mocks/photos';
+import PhotoList from './components/PhotoList';
 import './App.scss';
 
 
-
 const App = () => {
-  const threePhotos = photos.slice(0, 3);
 
   return (
     <div className="App">
-      {threePhotos.map((photo, index) => (
-        <PhotoListItem key={index} photo={photo} />
-      ))}
+      <PhotoList />
     </div>
   );
 };
