bc55933e57490eeacc79ff23c6f7d4c53623bdd3
Brendan Brown <brendan94brown@gmail.com>
2025-05-17 13:31:54 -0700

    Import mock data photos and topics, and update HomeRoute component to use incoming prop data

--- frontend/src/components/HomeRoute.jsx
+++ frontend/src/components/HomeRoute.jsx
@@ -4,12 +4,12 @@ import PhotoList from "./PhotoList";
 import '../styles/HomeRoute.scss'
 
 
-const HomeRoute = () => {
+const HomeRoute = ({ photos, topics }) => {
 
   return (
     <div className="home-route">
-      <TopNavigationBar />
-      <PhotoList />
+      <TopNavigationBar topics={topics} />
+      <PhotoList photos={photos} />
     </div>
   );
 };
