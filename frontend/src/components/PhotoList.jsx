c7003474137d2c92aa305f5bee1230b18b36768a
Brendan Brown <brendan94brown@gmail.com>
2025-05-17 13:32:56 -0700

    Remove sample data, update PhotoList component to accept prop data from photos

--- frontend/src/components/PhotoList.jsx
+++ frontend/src/components/PhotoList.jsx
@@ -1,70 +1,14 @@
-// frontend/src/components/PhotoList.jsx
 import React from 'react';
 import PhotoListItem from './PhotoListItem';
 import '../styles/PhotoList.scss';
 
 
-const sampleDataForPhotoList = [
-  {
-    id: "1",
-    location: {
-      city: "Montreal",
-      country: "Canada",
-    },
-    urls: {
-      full: "/Image-1-Full.jpeg",
-      regular: "/Image-1-Regular.jpeg",
-    },
-    user: {
-      id: "1",
-      username: "exampleuser",
-      name: "Joe Example",
-      profile: "/profile-1.jpg",
-    },
-  },
-  {
-    id: "2",
-    location: {
-      city: "Toronto",
-      country: "Canada",
-    },
-    urls: {
-      full: "/Image-2-Full.jpeg",
-      regular: "/Image-2-Regular.jpeg",
-    },
-    user: {
-      id: "2",
-      username: "exampleuser",
-      name: "Joe Example",
-      profile: "/profile-1.jpg",
-    },
-  },
-  {
-    id: "3",
-    location: {
-      city: "Ottawa",
-      country: "Canada",
-    },
-    urls: {
-      full: "/Image-3-Full.jpeg",
-      regular: "/Image-3-Regular.jpeg",
-    },
-    user: {
-      id: "3",
-      username: "exampleuser",
-      name: "Joe Example",
-      profile: "/profile-1.jpg",
-    },
-  },
-];
 
-
-
-const PhotoList = () => {
+const PhotoList = ({ photos }) => {
 
   return (
     <ul className="photo-list">
-      {sampleDataForPhotoList.map((photo) => (
+      {photos.map((photo) => (
         <li key={photo.id}>
           <PhotoListItem photo={photo} />
         </li>
