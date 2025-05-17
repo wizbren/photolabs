2f12dcd0f8b8cb91b6b7cd3636fbe44c989823a2
Brendan Brown <brendan94brown@gmail.com>
2025-05-17 12:45:45 -0700

    Create HomeRoute, add imports, build basic component structure, and export

---
+++ frontend/src/components/HomeRoute.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import TopNavigationBar from "./TopNavigationBar";
+import PhotoList from "./PhotoList";
+import '../styles/HomeRoute.scss'
+
+
+const HomeRoute = () => {
+
+  return (
+    <div className="home-route">
+      <TopNavigationBar />
+      <PhotoList />
+    </div>
+  );
+};
+
+
+export default HomeRoute;
