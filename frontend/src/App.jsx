2d00af2c1e4bc6428ae13bd78c23849da295a7cc
Brendan Brown <brendan94brown@gmail.com>
2025-05-16 21:04:32 -0700

    Import and render TopicList

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PhotoList from './components/PhotoList';
+import TopicList from './components/TopicList';
 import './App.scss';
 
 
@@ -7,6 +8,7 @@ const App = () => {
 
   return (
     <div className="App">
+      <TopicList />
       <PhotoList />
     </div>
   );
