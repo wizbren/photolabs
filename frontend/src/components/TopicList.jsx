830a9e987acf098cd6ef2b7fa7a1a2b13b78058c
Brendan Brown <brendan94brown@gmail.com>
2025-05-16 21:08:38 -0700

    Build out TopicList component to render the three TopicListItems with a map method, import TopicListItem

--- frontend/src/components/TopicList.jsx
+++ frontend/src/components/TopicList.jsx
@@ -1,4 +1,5 @@
 import "../styles/TopicList.scss";
+import TopicListItem from "./TopicListItem";
 
 const sampleDataForTopicList = [
   {
@@ -21,7 +22,9 @@ const sampleDataForTopicList = [
 const TopicList = () => {
   return (
     <div className="top-nav-bar__topic-list">
-      {/* Insert React */}
+      {sampleDataForTopicList.map((topic) => (
+        <TopicListItem key={topic.id} title={topic.title} />
+      ))}
     </div>
   );
 };
