8938c768963ec2f3e05bda6d0a961e1bf22cd115
Brendan Brown <brendan94brown@gmail.com>
2025-05-17 13:51:31 -0700

    Refactor TopicList component to accept prop data from topics; remove sample object that is no longer being used

--- frontend/src/components/TopicList.jsx
+++ frontend/src/components/TopicList.jsx
@@ -1,28 +1,12 @@
-import "../styles/TopicList.scss";
 import TopicListItem from "./TopicListItem";
+import "../styles/TopicList.scss";
 
-const sampleDataForTopicList = [
-  {
-    id: 1,
-    slug: "topic-1",
-    title: "Nature",
-  },
-  {
-    id: 2,
-    slug: "topic-2",
-    title: "Travel",
-  },
-  {
-    id: 3,
-    slug: "topic-3",
-    title: "People",
-  },
-];
 
-const TopicList = () => {
+
+const TopicList = ({ topics }) => {
   return (
     <div className="top-nav-bar__topic-list">
-      {sampleDataForTopicList.map((topic) => (
+      {topics.map((topic) => (
         <TopicListItem key={topic.id} title={topic.title} />
       ))}
     </div>
