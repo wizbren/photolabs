c405ea3df1711c3fbb890bf5a9fd016199687437
Brendan Brown <brendan94brown@gmail.com>
2025-05-17 13:53:21 -0700

    Refactor TopNavigationBar component to pass topics data to TopicList component

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -2,11 +2,11 @@ import TopicList from './TopicList';
 import FavBadge from './FavBadge';
 import '../styles/TopNavigationBar.scss'
 
-const TopNavigationBar = () => {
+const TopNavigationBar = ({ topics }) => {
   return (
     <div className="top-nav-bar">
       <span className="top-nav-bar__logo">PhotoLabs</span>
-      <TopicList />
+      <TopicList topics={topics}/>
       <FavBadge />
     </div>
   )
