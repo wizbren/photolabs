60f979b0b050f35a81db0ed95ceb1859ce9bfaf3
Brendan Brown <brendan94brown@gmail.com>
2025-05-18 11:51:52 -0700

    Add onClick event handler for closeModal, allow PhotoDetailsModal to receive closeModal as prop

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -1,10 +1,13 @@
 import '../styles/PhotoDetailsModal.scss'
 import closeSymbol from '../assets/closeSymbol.svg';
 
-const PhotoDetailsModal = () => {
+const PhotoDetailsModal = ({ closeModal }) => {
   return (
     <div className="photo-details-modal">
-      <button className="photo-details-modal__close-button">
+      <button 
+        className="photo-details-modal__close-button"
+        onClick={closeModal}
+        >
         <img src={closeSymbol} alt="close symbol" />
       </button>
     </div>
