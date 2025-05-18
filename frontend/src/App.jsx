5f1a7ad03aa9b2de4ba79ea33a26a196c425bca7
Brendan Brown <brendan94brown@gmail.com>
2025-05-17 19:58:25 -0700

    Import PhotoDetailsModal, add PhotoDetailsModal to App component, create openModal function for setting showModal state, and add short-circuit logic to PhotoDetailsModal component render

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import HomeRoute from './components/HomeRoute';
+import PhotoDetailsModal from './routes/PhotoDetailsModal';
 import photos from './mocks/photos';
 import topics from './mocks/topics';
 import './App.scss';
@@ -7,6 +8,7 @@ import './App.scss';
 
 const App = () => {
   const [favourites, setFavourites] = useState([]);
+  const [showModal, setShowModal] = useState(false)
 
   const toggleFavourite = (photoId) => {
     setFavourites((previous) => {
@@ -16,6 +18,11 @@ const App = () => {
     });
   };
 
+  const openModal = () => {
+    setShowModal(true)
+  };
+
+
   return (
     <div className="App">
       <HomeRoute
@@ -23,6 +30,7 @@ const App = () => {
         topics={topics}
         favourites={favourites}
         toggleFavourite={toggleFavourite} />
+      {showModal && <PhotoDetailsModal />}   {/* This might not be right!!! */}
     </div>
   );
 };
