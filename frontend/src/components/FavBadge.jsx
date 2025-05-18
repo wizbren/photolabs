f291e155bb43a3a9e2f5b5f7af8aa4f3a5daf6db
Brendan Brown <brendan94brown@gmail.com>
2025-05-17 18:58:04 -0700

    Update FavBadge component to turn FavIcon in TopNavBar red when a photo is favourited

--- frontend/src/components/FavBadge.jsx
+++ frontend/src/components/FavBadge.jsx
@@ -5,7 +5,7 @@ import '../styles/FavBadge.scss';
 const FavBadge = ({ isItFavourited }) => {
   return (
     <div className='fav-badge'>
-      <FavIcon displayAlert={!!isItFavourited}/>
+      <FavIcon displayAlert={isItFavourited} selected={isItFavourited} />
     </div>
   ) 
 };
