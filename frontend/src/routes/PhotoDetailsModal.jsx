a432412e832fe7adc499d35ade4db1cc3dd499d3
Brendan Brown <brendan94brown@gmail.com>
2025-05-18 15:34:03 -0700

    Add logic to show similar photos in modal, fix typos, add method to convert similar_photos object, add two placeholder functions to prevent crashing

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -6,26 +6,40 @@ import PhotoList from '../components/PhotoList';
 const PhotoDetailsModal = ({ closeModal, photo }) => {
   console.log("Photo opened in modal:", photo);
 
+  //* Convert object to array (MIGHT NOT BE RIGHT)
+  const similarPhotosArray = Object.values(photo.similar_photos);
+
   return (
     <div className="photo-details-modal">
-
-      <button 
+      {/* Close button */}
+      <button
         className="photo-details-modal__close-button"
         onClick={closeModal}
-        >
+      >
         <img src={closeSymbol} alt="close symbol" />
       </button>
-      
+      {/* Header */}
       <h2 className="photo-details-modal__header">
         Photo b {photo.user.name} from {photo.location.city}, {photo.location.country}
       </h2>
-
+      {/* Enlarged selected image */}
       <div className="photo-details-modal__images">
         <img
           className="photo-details-modal__image"
-          src="{photo.urls.full"
+          src={photo.urls.full}
           alt="Enlarged selected photo"
         />
+      </div>
+      {/* Similar photos section */}
+      <div className="photo-details-modal__similar-photos">
+        <h3>Similar Photos</h3>
+        <PhotoList
+          photos={similarPhotosArray}
+          favourites={[]}
+          toggleFavourite={() => { }}    // PLACEHOLDER
+          openModal={() => { }}          // PLACEHOLDER
+        />
+
       </div>
     </div>
   )
