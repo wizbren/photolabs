a248030161d855e8825c679fba6688bdcac2719c
Brendan Brown <brendan94brown@gmail.com>
2025-05-18 12:34:35 -0700

    Add logic to display selected photos and headers correctly in the modal

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -1,5 +1,6 @@
 import '../styles/PhotoDetailsModal.scss'
 import closeSymbol from '../assets/closeSymbol.svg';
+import PhotoList from '../components/PhotoList';
 
 
 const PhotoDetailsModal = ({ closeModal, photo }) => {
@@ -7,12 +8,25 @@ const PhotoDetailsModal = ({ closeModal, photo }) => {
 
   return (
     <div className="photo-details-modal">
+
       <button 
         className="photo-details-modal__close-button"
         onClick={closeModal}
         >
         <img src={closeSymbol} alt="close symbol" />
       </button>
+      
+      <h2 className="photo-details-modal__header">
+        Photo b {photo.user.name} from {photo.location.city}, {photo.location.country}
+      </h2>
+
+      <div className="photo-details-modal__images">
+        <img
+          className="photo-details-modal__image"
+          src="{photo.urls.full"
+          alt="Enlarged selected photo"
+        />
+      </div>
     </div>
   )
 };
