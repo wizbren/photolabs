9b53f21b03cc7edb5548b8bf46fb90cac3ba28b7
Brendan Brown <brendan94brown@gmail.com>
2025-05-18 17:27:14 -0700

    Refactor PhotoListItem so layout groups image and icon

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -7,12 +7,14 @@ const PhotoListItem = ({ photo, isFavourite, toggleFavourite, openModal }) => {
 
   return (
     <div className="photo-list__item">
-      <PhotoFavButton
-        photoId={photo.id}
-        isFavourite={isFavourite}
-        toggleFavourite={toggleFavourite}
-      />
-      <img className="photo-list__image" src={photo.urls.regular} alt="Main" onClick={() => openModal(photo)} />
+      <div className="photo-list__image-container">
+        <PhotoFavButton
+          photoId={photo.id}
+          isFavourite={isFavourite}
+          toggleFavourite={toggleFavourite}
+        />
+        <img className="photo-list__image" src={photo.urls.regular} alt="Main" onClick={() => openModal(photo)} />
+      </div>
       <div className="photo-list__user-details">
         <img className="photo-list__user-profile" src={photo.user.profile} alt="User profile" />
         <div className="photo-list__user-info">
