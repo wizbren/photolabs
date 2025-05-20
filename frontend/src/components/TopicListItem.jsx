2a9df5ffce801e8426c6c18ebc4b85f210ec4013
Brendan Brown <brendan94brown@gmail.com>
2025-05-20 15:46:29 -0700

    Remove unused sample object, remove one empty line

--- frontend/src/components/TopicListItem.jsx
+++ frontend/src/components/TopicListItem.jsx
@@ -1,10 +1,6 @@
 import "../styles/TopicListItem.scss";
 
-const sampleDataForTopicListItem = {
-  id: 1,
-  slug: "topic-1",
-  label: "Nature",
-};
+
 
 const TopicListItem = ({ title, onClick }) => {
   return (
