e3116e02e70f45f7dfd287f06e519c2e4640af76
Brendan Brown <brendan94brown@gmail.com>
2025-05-20 09:30:30 -0700

    Fix App to correctly pass fetchPhotosByTopic to HomeRoute

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,7 +11,8 @@ const App = () => {
     state,
     updateFavPhotoIds,
     onPhotoSelect,
-    closePhotoModal
+    closePhotoModal,
+    fetchPhotosByTopic
   } = useApplicationData();
 
   return (
