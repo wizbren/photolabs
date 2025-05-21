bf5fd2b217828028d771ba6dbbdea3975b9e9b77
Brendan Brown <brendan94brown@gmail.com>
2025-05-20 20:51:37 -0700

    Pass onViewFavourites prop into function, add button to trigger view toggle next to FavBadge

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -2,13 +2,21 @@ import TopicList from './TopicList';
 import FavBadge from './FavBadge';
 import '../styles/TopNavigationBar.scss'
 
-const TopNavigationBar = ({ topics, favourites, onTopicClick }) => {
+const TopNavigationBar = ({
+  topics,
+  favourites,
+  onTopicClick,
+  onViewFavourites }) => {
+
   const isItFavourited = favourites.length > 0;
 
   return (
     <div className="top-nav-bar">
       <span className="top-nav-bar__logo">PhotoLabs</span>
       <TopicList topics={topics} onTopicClick={onTopicClick} />
+      <button onClick={onViewFavourites}>
+        View Favourites
+      </button>
       <FavBadge isItFavourited={isItFavourited} />
     </div>
   )
